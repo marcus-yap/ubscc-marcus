9d5cda652d62ca51ba0f28b334c2392bd6a3c160
MARCUS YAP <yap.marcus@yahoo.com.sg>
2025-09-06 22:40:53 +0800

    2048

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,3 +5,12 @@ import routes.square
 import routes.ticketing_agent
 import routes.trading_formula
 import routes.trivia
+
+import importlib.util, os, sys
+
+_2048_path = os.path.join(os.path.dirname(__file__), "2048.py")
+if os.path.exists(_2048_path):
+    spec = importlib.util.spec_from_file_location("routes._2048", _2048_path)
+    _mod = importlib.util.module_from_spec(spec)
+    sys.modules["routes._2048"] = _mod
+    spec.loader.exec_module(_mod)
