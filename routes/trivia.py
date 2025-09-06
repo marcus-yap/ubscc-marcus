6b93adaf1bc08cc288ed6d9f475f0a6cd0253847
MARCUS YAP <yap.marcus@yahoo.com.sg>
2025-09-06 22:14:06 +0800

    trivia fix

--- routes/trivia.py
+++ routes/trivia.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 
 @app.route('/trivia', methods=['POST'])
 def trivia():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    result = {"answers": [3, 1, 2, 2, 3, 4, 1, 5, 4, 3, 3, 2, 4, 4, 2, 2, 1, 2, 4, 3, 1, 2, 2, 4, 1]}
-    logging.info("My result :{}".format(result))
+    result = {
+        "answers": [3, 1, 2, 2, 3, 4, 1, 5, 4, 3, 3, 2, 4, 4, 2, 2, 1, 2, 4, 3, 1, 2, 2, 4, 1]
+    }
+    logging.info("My result :%s", result)
     return json.dumps(result)
